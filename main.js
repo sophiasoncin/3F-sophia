e6bcb96041a71ac5cf295981adf0c4045b711fef
sophiasoncin <110099928+sophiasoncin@users.noreply.github.com>
2024-06-20 09:41:48 -0300

    20/06/2024

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
-contador[0].textContent="ola"
 const tempoObjetivo1 = new Date (2024,11,12,7,53)
 const tempoObjetivo2 = new Date (2025,05,14,12,30)
 const tempoObjetivo3 = new Date (2024,06,14,19,00)
 const tempoObjetivo4 = new Date (2025,04,29,14,05)
+let agora = new Date();
+contador[0].textContent = tempoObjetivo1-agora
+contador[1].textContent = tempoObjetivo2-agora
+contador[2].textContent = tempoObjetivo3-agora
+contador[3].textContent = tempoObjetivo4-agora
 
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
