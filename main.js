5f48d9b475b3922c4b589bf486114408df7a8ca3
sophiasoncin <110099928+sophiasoncin@users.noreply.github.com>
2024-09-12 09:40:57 -0300

    12/09/2024

--- main.js
+++ main.js
@@ -42,11 +42,11 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0]
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1]
-    document.getElementById("minutos0").textContent = calculaTempo(tempos[0])[2]
-    document.getElementById("segundos0").textContent = calculaTempo(tempos[0])[3]
-     for (let i=0; i<contadores.length;i++){
+    for (let i=0; i<contadores.length;i++){
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0]
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1]
+    document.getElementById("minutos"+i).textContent = calculaTempo(tempos[i])[2]
+    document.getElementById("segundos"+i).textContent = calculaTempo(tempos[i])[3]
         //contadores[i].textContent = calculaTempo(tempos[i]);
       
     }
